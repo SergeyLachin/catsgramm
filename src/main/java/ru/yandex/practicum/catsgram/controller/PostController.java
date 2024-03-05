465016753574eb70d8e39fba4f24a0c789d45fc6
15151515 <kaz.sergejs@yandex.by>
2024-03-05 22:37:24 +0300

    cats

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.service.PostService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/posts")
+    public List<Post> findAll() {
+        return postService.findAll();
+    }
+
+    @PostMapping(value = "/post")
+    public Post create(@RequestBody Post post) {
+        return postService.create(post);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Optional<Post> findById(@PathVariable int postId) {
+        return postService.findById(postId);
+    }
+
+}
---
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.catsgram.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private final List<Post> posts = new ArrayList<>();
+
+    public List<Post> findAll() {
+        return posts;
+    }
+
+    public Post create(Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    public Optional<Post> findById(int postId) {
+        return posts.stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst();
+    }
+}
